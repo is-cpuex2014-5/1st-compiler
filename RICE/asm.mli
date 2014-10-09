5ad8f2a90a7a0dd164daad935a51f92b3f430b60
Sakas <kensakayori@gmail.com>
2014-10-10 00:20:25 +0900

    changed asm.mli

--- RICE/asm.mli
+++ RICE/asm.mli
@@ -2,31 +2,34 @@ type id_or_imm = V of Id.t | C of int
 type t = 
   | Ans of exp
   | Let of (Id.t * Type.t) * exp * t
-and exp = 
+and exp = (* 一つ一つの命令に対応する式 *) (*抜けている命令も多い*) (*現状論理命令がごっそりない*)
   | Nop
   | Li of int
-  | FLi of Id.l
-  | SetL of Id.l
-  | Mr of Id.t
+  | FLi of Id.l (*必要なのか*)
+  | SetL of Id.l (*Set Label*)
+  | Mov of Id.t
   | Neg of Id.t
-  | Add of Id.t * id_or_imm
-  | Sub of Id.t * id_or_imm
-  | Slw of Id.t * id_or_imm
-  | Lwz of Id.t * id_or_imm
-  | Stw of Id.t * Id.t * id_or_imm
-  | FMr of Id.t 
+  | Add of Id.t * Id.t * id_or_imm
+  | Sub of Id.t * Id.t
+  | Sll of Id.t * id_or_imm
+  | Srl of Id.t * id_or_imm
+  | Sla of Id.t * id_or_imm
+  | Sra of Id.t * id_or_imm
+  | Load of Id.t * Id.t *  id_or_imm
+  | Store of Id.t * Id.t * id_or_imm
+  | FMov of Id.t  (*ftoi, itofがない状態*)
   | FNeg of Id.t
   | FAdd of Id.t * Id.t
   | FSub of Id.t * Id.t
   | FMul of Id.t * Id.t
   | FDiv of Id.t * Id.t
-  | Lfd of Id.t * id_or_imm
-  | Stfd of Id.t * Id.t * id_or_imm
+  | Fload of Id.t * Id.t * id_or_imm
+  | FStore of Id.t * Id.t * id_or_imm
   | Comment of string
   (* virtual instructions *)
   | IfEq of Id.t * id_or_imm * t * t
   | IfLE of Id.t * id_or_imm * t * t
-  | IfGE of Id.t * id_or_imm * t * t (* for simm *)
+  | IfGE of Id.t * id_or_imm * t * t
   | IfFEq of Id.t * Id.t * t * t
   | IfFLE of Id.t * Id.t * t * t
   (* closure address, integer arguments, and float arguments *)
