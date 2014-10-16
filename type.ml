8c34647008fbf4f1a0542286eeb2825f120c8866
Sakas <kensakayori@gmail.com>
2014-10-16 23:49:49 +0900

    Fixed type.ml

--- type.ml
+++ type.ml
@@ -9,23 +9,6 @@ type t = (* MinCamlの型を表現するデータ型 (caml2html: type_t) *)
   | Var of t option ref
 
 let gentyp () = Var(ref None) (* 新しい型変数を作る *)
-
-
-
-let rec p oc = function 
-  | Unit -> Printf.fprintf oc "unit"
-  | Bool -> Printf.fprintf oc "bool"
-  | Int -> Printf.fprintf oc "int"
-  | Float -> Printf.fprintf oc "float"
-  | Fun (l, t) -> Printf.fprintf oc "fun ";
-		List.iter (fun x -> p oc x; Printf.fprintf oc "  -> ") l;
-		p oc t
-  | Tuple l -> Printf.fprintf oc "tuple ";
-	       List.iter (fun x -> p oc x; Printf.fprintf oc " ") l
-  | Array t -> Printf.fprintf oc "array "; p oc t
-  | Var r -> match !r with
-	     | None -> Printf.fprintf oc "tvar"
-	     | Some t -> p oc t
 		      
 let rec sprint  = function 
   | Unit ->  "unit"
@@ -35,10 +18,12 @@ let rec sprint  = function
   | Fun (l, t) -> "fun " ^
 		    (List.fold_left (fun x y -> x ^ (sprint y) ^  "  -> ") "" l) ^
 		      sprint  t
-  | Tuple l ->  "tuple " ^
-	       (List.fold_left (fun x y -> x ^ (sprint  y) ^ " ") "" l)
-  | Array t -> "array " ^ (sprint  t)
+  | Tuple l ->  "tuple (" ^
+	       (List.fold_left (fun x y -> x ^ (sprint  y) ^ " ") "" l) ^
+		 ")"
+  | Array t -> "array (" ^ (sprint  t) ^ ")"
   | Var r -> match !r with
 	     | None -> "tvar"
 	     | Some t -> sprint  t
 		      
+let rec p oc t = Printf.fprintf oc "%s" (sprint t)
