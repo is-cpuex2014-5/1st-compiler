0bb522fe29270bb708077b82ea1f5413995b60b9
Sakas <kensakayori@gmail.com>
2015-01-24 18:53:05 +0900

    Updated kNormal.mli

--- kNormal.mli
+++ kNormal.mli
@@ -30,5 +30,5 @@ and fundef = { name : Id.t * Type.t; args : (Id.t * Type.t) list; body : t }
 
 val fv : t -> S.t
 val f : Syntax.t -> t
-(*val p : out_channel -> t -> t*)
+val p : out_channel -> t -> t
 
