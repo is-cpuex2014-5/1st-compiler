459c5d30de4cc7fdc3e8169cf83e5e0aafee0d0a
Sakas <kensakayori@gmail.com>
2015-03-10 18:26:21 +0900

    clean up

--- ._d/main.d
+++
@@ -1,6 +0,0 @@
-main.cmo: virtual.cmi typing.cmi simm.cmi regAlloc.cmi parser.cmi m.cmo \
-    lexer.cmo kNormal.cmi inline.cmi id.cmo emit.cmi elim.cmi constFold.cmi \
-    closure.cmi beta.cmi assoc.cmi alpha.cmi main.cmi
-main.cmx: virtual.cmi typing.cmx simm.cmi regAlloc.cmi parser.cmx m.cmx \
-    lexer.cmx kNormal.cmx inline.cmx id.cmx emit.cmi elim.cmx constFold.cmx \
-    closure.cmx beta.cmx assoc.cmx alpha.cmx main.cmi
